3dc4eeca41e2eff145423fa8903d4483010f8105
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-02 15:43:15 +0530

    Update sam75.c

--- sam75.c
+++ sam75.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 #define max 1000
 void main()
 {
-int i;
-char str;
+int i,loo;
+char str[max];
 printf("\nenter string");
 scanf("%s",&str);
 i=strlen(str);
+if(i%2!=0)
+{
 i/=2;
 str[i]='*';
 printf("%s",str);
+}
+else
+{
+    i/=2;
+    str[i]='*';
+    --i;
+    str[i]='*';
+    printf("%s",str);
+}
 }
